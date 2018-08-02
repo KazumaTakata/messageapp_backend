1360dfac85d486c53165ef2707a35d2026105856
Kazuma Takata <neilbuzztakata@gmail.com>
2018-08-03 00:41:31 +0900

    db module without test

--- db/database.js
+++ db/database.js
@@ -1,27 +1,29 @@
 var MongoClient = require("mongodb").MongoClient;
 var mongo = require("mongodb");
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://localhost:8899/";
 
 var errHandler = function(err) {
   console.log(err);
 };
 
-let connectToDatabase = new Promise(function(resolve, reject) {
-  MongoClient.connect(url, function(err, db) {
-    if (err) {
-      reject(err);
-    } else {
-      let dbo = db.db("swiftline");
-      let collection = dbo.collection("users");
-      collection.db = db;
-      resolve(collection);
-    }
+let connectToDatabase = function(Cname) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      if (err) {
+        reject(err);
+      } else {
+        let dbo = db.db("swiftline");
+        let collection = dbo.collection(Cname);
+        collection.db = db;
+        resolve(collection);
+      }
+    });
   });
-});
+};
 
 function findUserById(id) {
   return new Promise((resolve, reject) => {
-    connectToDatabase.then(conn => {
+    connectToDatabase("users").then(conn => {
       conn.findOne({ _id: new mongo.ObjectID(id) }, function(err, result) {
         resolve(result);
         conn.db.close();
@@ -30,8 +32,132 @@ function findUserById(id) {
   });
 }
 
+function updateOneField(id, fieldname, fieldvalue) {
+  return new Promise((resolve, reject) => {
+    connectToDatabase("users").then(conn => {
+      let updateData = {};
+      updateData[fieldname] = fieldvalue;
+      conn.updateOne(
+        { _id: new mongo.ObjectID(id) },
+        {
+          $set: updateData,
+        }
+      );
+    });
+  });
+}
+
+function addFriend(myid, friendid) {
+  return new Promise((resolve, reject) => {
+    connectToDatabase("users").then(conn => {
+      var o_id = new mongo.ObjectID(myid);
+      let f_id = new mongo.ObjectID(friendid);
+
+      conn.updateOne(
+        { _id: f_id },
+        {
+          $push: { friendIds: o_id },
+        },
+        (err, result) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(result);
+        }
+      );
+    }, reject);
+  });
+}
+
+function findUserByName(name) {
+  return new Promise((resolve, reject) => {
+    connectToDatabase("users").then(conn => {
+      conn.findOne({ name: name }, function(err, result) {
+        resolve(result);
+        conn.db.close();
+      });
+    }, reject);
+  });
+}
+
+function findUsersById(ids) {
+  return new Promise((resolve, reject) => {
+    connectToDatabase("users").then(conn => {
+      conn.findOne({ _id: { $in: ids } }, function(err, result) {
+        resolve(result);
+        conn.db.close();
+      });
+    }, reject);
+  });
+}
+
+function insertUser(name, password) {
+  let obj = {
+    name: name,
+    password: password,
+    photourl: "http://localhost:8181/img/defaultprofile.png",
+    friendIds: [],
+    talks: [],
+    backgroundurl: "http://localhost:8181/img/rocco-caruso-722282-unsplash.jpg",
+  };
+
+  return new Promise((resolve, reject) => {
+    connectToDatabase("users").then(conn => {
+      conn.insertOne(obj, function(err, result) {
+        if (err) {
+          reject(err);
+        }
+        console.log("1 document inserted");
+        resolve(result);
+        conn.db.close();
+      });
+    });
+  });
+}
+
+function destroyAll() {
+  return new Promise((resolve, reject) => {
+    connectToDatabase("users").then(conn => {
+      conn.deleteMany({}, function(err, result) {
+        if (err) {
+          reject(err);
+        }
+        console.log("delete all");
+        resolve(result);
+        conn.db.close();
+      });
+    });
+  });
+}
+
+function getStoredTalk(id) {
+  return new Promise((resolve, reject) => {
+    connectToDatabase("users").then(conn => {
+      conn.findOne({ _id: new mongo.ObjectID(id) }, function(err, result) {
+        if (err) {
+          reject(err);
+        }
+        resolve(result.talks);
+        conn.updateOne(
+          { _id: new mongo.ObjectID(id) },
+          { $set: { talks: [] } }
+        );
+      });
+    });
+  });
+}
+
 // findUserById("5b35d1b317f95911e0fad272").then(result => {
 //   console.log(result);
 // }, errHandler);
 
-module.exports = { findUserById };
+module.exports = {
+  findUserById,
+  findUsersById,
+  insertUser,
+  destroyAll,
+  findUserByName,
+  addFriend,
+  getStoredTalk,
+  updateOneField,
+};
--- test/db/database.test.js
+++ test/db/database.test.js
@@ -1,10 +1,17 @@
 let functions = require("../../db/database");
 
+beforeEach(async () => {
+  await functions.destroyAll();
+  await functions.insertUser("sample_user1", "sample_password1");
+  await functions.insertUser("sample_user2", "sample_password2");
+});
+
+afterEach(async () => {
+  await functions.destroyAll();
+});
+
 test("find feuser by id", done => {
-  expect(
-    functions.findUserById("5b35d1b317f95911e0fad272").then(result => {
-      done();
-      console.log(result);
-    }, console.log)
-  );
+  functions.insertUser("sample_user1", "sample_password1").then(result => {
+    done();
+  }, console.log);
 });
