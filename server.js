b66e7f8762bfb569225a07ad86a1e63d1d1617be
Kazuma Takata <neilbuzztakata@gmail.com>
2018-09-02 04:52:19 +0900

    refactor websocket

--- server.js
+++ server.js
@@ -19,86 +19,80 @@ wss.on("connection", function connection(ws) {
     delete socketPool[key];
   });
 
-  ws.on("message", function incoming(message) {
-    console.log(message);
+  ws.on("message", message => {
+    console.log("received: %s", message);
     let token = JSON.parse(message.toString()).myId;
 
     jwt.verify(token, config.secret, async function(err, decoded) {
       let myId = decoded.id;
-      let ping = JSON.parse(message.toString()).ping;
+      let type = JSON.parse(message.toString()).type;
+      let friendId = JSON.parse(message.toString()).friendId;
+      let content = JSON.parse(message.toString()).content;
+      let time = JSON.parse(message.toString()).time;
+      let groupid = JSON.parse(message.toString()).groupid;
+      let chatindex = JSON.parse(message.toString()).chatindex;
 
-      if (ping == "hey") {
+      if (type == "ping") {
         socketPool[myId] = ws;
-      } else {
-        let friendId = JSON.parse(message.toString()).friendId;
-        let content = JSON.parse(message.toString()).content;
-        let time = JSON.parse(message.toString()).time;
-        let groupid = JSON.parse(message.toString()).groupid;
-        let chatindex = JSON.parse(message.toString()).chatindex;
+      } else if (type == "grouptalk") {
         socketPool[myId] = ws;
-        console.log("received: %s", message);
-        // wss.clients.forEach( (client) => {
-        //     client.send(message)
-        // });
-        if (groupid != undefined) {
-          let group = await database.findGroupById(groupid);
-          group.member.forEach(async member => {
-            if (member != myId) {
-              if (socketPool[member] != undefined) {
-                try {
-                  let payload = { content, id: myId, groupid, time, chatindex };
-                  socketPool[friendId].send(JSON.stringify(payload));
-                } catch (e) {
-                  console.log(e);
-                }
+        let group = await database.findGroupById(groupid);
+        group.member.forEach(async member => {
+          if (member != myId) {
+            let socket = socketPool[member];
+            if (socket != undefined) {
+              try {
+                let payload = { content, id: myId, groupid, time, chatindex };
+                socket.send(JSON.stringify(payload));
+              } catch (e) {
+                console.log(e);
               }
             }
-          });
-          let insertobj = { content, senderid: myId, time };
-          if (chatindex != undefined) {
-            await database.insertTalkToGroupAsResponce(
-              groupid,
-              chatindex,
-              insertobj
-            );
-          } else {
-            await database.insertTalkToGroup(groupid, insertobj);
-            let elasticobj = { senderid: myId, time, content, groupid };
-            await elasticgroup.addDocument(elasticobj);
+          }
+        });
+        let insertobj = { content, senderid: myId, time };
+        if (chatindex != undefined) {
+          await database.insertTalkToGroupAsResponce(
+            groupid,
+            chatindex,
+            insertobj
+          );
+        } else {
+          await database.insertTalkToGroup(groupid, insertobj);
+          let elasticobj = { senderid: myId, time, content, groupid };
+          await elasticgroup.addDocument(elasticobj);
+        }
+      } else if (type == "talk") {
+        if (socketPool[friendId] != undefined) {
+          try {
+            let payload = { content: content, id: myId, time: time };
+            socketPool[friendId].send(JSON.stringify(payload));
+          } catch (e) {
+            console.log(e);
           }
         } else {
-          if (socketPool[friendId] != undefined) {
-            try {
-              let payload = { content: content, id: myId, time: time };
-              socketPool[friendId].send(JSON.stringify(payload));
-            } catch (e) {
-              console.log(e);
-            }
-          } else {
-            await database.insertTalk(myId, friendId, content, time);
-          }
-          await database.insertTalkAll(myId, friendId, content, time, 1);
-          await database.insertTalkAll(friendId, myId, content, time, 0);
-
-          let insertobj = {
-            userid: myId,
-            friendid: friendId,
-            content: content,
-            time: time,
-            which: true,
-          };
-          await elastic.addDocument(insertobj);
-          let insertobj2 = {
-            userid: friendId,
-            friendid: myId,
-            content: content,
-            time: time,
-            which: false,
-          };
-          await elastic.addDocument(insertobj2);
+          await database.insertTalk(myId, friendId, content, time);
         }
+        await database.insertTalkAll(myId, friendId, content, time, 1);
+        await database.insertTalkAll(friendId, myId, content, time, 0);
+
+        let insertobj = {
+          userid: myId,
+          friendid: friendId,
+          content: content,
+          time: time,
+          which: true,
+        };
+        await elastic.addDocument(insertobj);
+        let insertobj2 = {
+          userid: friendId,
+          friendid: myId,
+          content: content,
+          time: time,
+          which: false,
+        };
+        await elastic.addDocument(insertobj2);
       }
     });
   });
-  //   ws.send('something');
 });
