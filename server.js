6a0e60ac639af383ad5971708bdd56db0b99450c
Kazuma Takata <neilbuzztakata@gmail.com>
2018-09-02 16:47:18 +0900

    group login logout notification

--- server.js
+++ server.js
@@ -10,13 +10,56 @@ var elasticgroup = require("./elasticsearch/grouptalk");
 const wss = new WebSocket.Server({ port: 8084 });
 let socketPool = {};
 
+var uniqueArray = function(arrArg) {
+  return arrArg.filter(function(elem, pos, arr) {
+    return arr.indexOf(elem) == pos;
+  });
+};
+
 wss.on("connection", function connection(ws) {
   console.log("connect");
 
-  ws.on("close", function close() {
+  ws.on("close", async () => {
     console.log("closed");
-    let key = Object.keys(socketPool).find(key => socketPool[key] === this);
+    let key = Object.keys(socketPool).find(key => socketPool[key] === ws);
     delete socketPool[key];
+
+    if (key != undefined) {
+      let user = await database.findUserById(key);
+      let friendids = user.friendIds;
+      let groupids = user.groups;
+      let allloginusers = Object.keys(socketPool);
+      let loginusers = friendids.filter(id => {
+        return allloginusers.includes(id.toString());
+      });
+
+      for (let i = 0; i < loginusers.length; i++) {
+        socketPool[loginusers[i]].send(
+          JSON.stringify({
+            type: "logout",
+            logoutuserid: key,
+          })
+        );
+      }
+
+      let groupids_obj = groupids.map(id => new mongo.ObjectID(id));
+      let groups = await database.getGroup(groupids_obj);
+      let groupmembers = groups.map(g => g.member);
+      let groupmembers_flatten = [].concat(...groupmembers);
+      let groupmembers_flatten_unique = uniqueArray(groupmembers_flatten);
+      let logingroupusers = groupmembers_flatten_unique.filter(id => {
+        return allloginusers.includes(id.toString());
+      });
+
+      for (let i = 0; i < logingroupusers.length; i++) {
+        socketPool[logingroupusers[i]].send(
+          JSON.stringify({
+            type: "logoutgroup",
+            logoutgroupuserid: key,
+          })
+        );
+      }
+    }
   });
 
   ws.on("message", message => {
@@ -34,6 +77,50 @@ wss.on("connection", function connection(ws) {
 
       if (type == "ping") {
         socketPool[myId] = ws;
+        let user = await database.findUserById(myId);
+        let friendids = user.friendIds;
+        let groupids = user.groups;
+        let allloginusers = Object.keys(socketPool);
+        let loginusers = friendids.filter(id => {
+          return allloginusers.includes(id.toString());
+        });
+        let sentobj = { type: "loginuserlist", loginfriendids: loginusers };
+        ws.send(JSON.stringify(sentobj));
+
+        for (let i = 0; i < loginusers.length; i++) {
+          socketPool[loginusers[i]].send(
+            JSON.stringify({
+              type: "newloginuser",
+              loginuserid: myId,
+            })
+          );
+        }
+
+        let groupids_obj = groupids.map(id => new mongo.ObjectID(id));
+        let groups = await database.getGroup(groupids_obj);
+        let groupmembers = groups.map(g => g.member);
+        let groupmembers_flatten = [].concat(...groupmembers);
+        let groupmembers_flatten_unique = uniqueArray(groupmembers_flatten);
+        console.log(uniqueArray(groupmembers_flatten_unique));
+        let logingroupusers = groupmembers_flatten_unique.filter(id => {
+          return allloginusers.includes(id.toString());
+        });
+        let sentobj2 = {
+          type: "logingroupuserlist",
+          logingroupfriendids: logingroupusers,
+        };
+        ws.send(JSON.stringify(sentobj2));
+
+        for (let i = 0; i < logingroupusers.length; i++) {
+          if (logingroupusers[i] != myId) {
+            socketPool[logingroupusers[i]].send(
+              JSON.stringify({
+                type: "newlogingroupuser",
+                logingroupuserid: myId,
+              })
+            );
+          }
+        }
       } else if (type == "grouptalk") {
         socketPool[myId] = ws;
         let group = await database.findGroupById(groupid);
@@ -42,7 +129,14 @@ wss.on("connection", function connection(ws) {
             let socket = socketPool[member];
             if (socket != undefined) {
               try {
-                let payload = { content, id: myId, groupid, time, chatindex };
+                let payload = {
+                  type: "newgroupchat",
+                  content,
+                  id: myId,
+                  groupid,
+                  time,
+                  chatindex,
+                };
                 socket.send(JSON.stringify(payload));
               } catch (e) {
                 console.log(e);
@@ -65,7 +159,12 @@ wss.on("connection", function connection(ws) {
       } else if (type == "talk") {
         if (socketPool[friendId] != undefined) {
           try {
-            let payload = { content: content, id: myId, time: time };
+            let payload = {
+              type: "newchat",
+              content: content,
+              id: myId,
+              time: time,
+            };
             socketPool[friendId].send(JSON.stringify(payload));
           } catch (e) {
             console.log(e);
